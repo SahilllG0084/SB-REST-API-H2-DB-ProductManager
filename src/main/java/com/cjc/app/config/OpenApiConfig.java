fe2caa07927c158cf6fdd7ac219096c597ff83f5
Sahil Gurav <sahilgurav8440@gmail.com>
2025-12-31 15:12:22 +0530

    config changed

--- src/main/java/com/cjc/app/config/OpenApiConfig.java
+++ src/main/java/com/cjc/app/config/OpenApiConfig.java
@@ -2,13 +2,24 @@ package com.cjc.app.config;
 
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 @Configuration
 @OpenAPIDefinition(
         info = @Info(
                 title = "Product Management REST API",
-                version = "1.0",
-                description = "Spring Boot REST API Documentation"
+                version = "1.0.0",
+                description = "REST API for managing products with CRUD Operation, pagination, sorting, searching and filtering",
+                contact = @Contact(
+                        name = "Sahil Gurav",
+                        email = "sahil@example.com",
+                        url = "https://github.com/SahilllG0084"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                )
         )
 )
 public class OpenApiConfig {
